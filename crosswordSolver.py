c94b96fc3b22c818216a770b2d9d4e01633623a2
Mali <altunsoymali@gmail.com>
2020-12-12 13:48:19 +0300

    wordLengthCalculator function added

---
+++ crosswordSolver.py
@@ -0,0 +1,68 @@
+
+class CrosswordSolver:
+    def __init__(self, grid, numbers, downClues, acrossClues):
+        self.grid = grid
+        self.numbers = numbers
+        self.downClues = downClues
+        self.acrossClues = acrossClues
+
+        self.lengthOfDownClues = []
+        self.lengthOfAcrossClues = []
+        self.wordLengthCalculator()
+        
+    def wordLengthCalculator(self):
+        #downClues
+        for clue in self.downClues:
+            clueNumber = clue[0]
+            rowIndex = -1
+            colIndex = -1
+            counter = 0
+            for row in self.numbers:
+                if clueNumber in row:
+                    rowIndex = counter
+                counter = counter + 1
+            colIndex = self.numbers[rowIndex].index(clueNumber)
+
+            #count spaces
+            wordLength = 0
+            for row in self.grid[rowIndex:]:
+                if row[colIndex] == "0":
+                    wordLength = wordLength + 1
+            self.lengthOfDownClues.append([clueNumber, wordLength])
+
+            #acrossClues
+        for clue in self.acrossClues:
+            clueNumber = clue[0]
+            rowIndex = -1
+            colIndex = -1
+            counter = 0
+            for row in self.numbers:
+                if clueNumber in row:
+                    rowIndex = counter
+                counter = counter + 1
+            colIndex = self.numbers[rowIndex].index(clueNumber)
+
+            #count spaces
+            wordLength = 0
+            for cell in self.grid[rowIndex][colIndex:]:
+                if cell == "0":
+                    wordLength = wordLength + 1
+            self.lengthOfAcrossClues.append([clueNumber, wordLength])
+
+
+            
+
+
+
+
+
+textFromWeb = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+
+
+
+grid = [["1","0","0","0","0"],["1","0","0","0","0"],["0","0","0","0","0"],["0","0","0","0","1"],["0","0","0","0","1"]]
+numbers = [["-","1","2","3","4"],["-","5","-","-","-"],["6","-","-","-","-"],["7","-","-","-","-"],["8","-","-","-","-"]]
+downClues = [['1', 'Partially melted snow'],['2', 'New employee'],['3', 'Result of a successful job interview'],['4', 'Director Anderson'],['6', '[not my mistake]']]
+acrossClues = [['1', '"Brooklyn Nine-Nine" or "The King of Queens"'],['5', '"What happens to us while we are making other plans," per Allen Saunders'],['6', 'Enjoys Santa Monica, perhaps'],['7', '"It\'s all clear now"'],['8', 'Singer nicknamed the "Goddess of Pop"']]
+
+sovler = CrosswordSolver(grid, numbers, downClues, acrossClues)
