d52945a6062ffe26eb63ef99e7ddec693c7bb138
Mali <altunsoymali@gmail.com>
2020-12-12 14:11:26 +0300

    merge with main

--- crosswordSolver.py
+++ crosswordSolver.py
@@ -9,6 +9,12 @@ class CrosswordSolver:
         self.lengthOfDownClues = []
         self.lengthOfAcrossClues = []
         self.wordLengthCalculator()
+
+        self.downClueDomains = []
+        self.acrossClueDomains = []
+        self.webScrap()
+        self.filterDomains()
+
         
     def wordLengthCalculator(self):
         #downClues
@@ -48,15 +54,33 @@ class CrosswordSolver:
                 if cell == "0":
                     wordLength = wordLength + 1
             self.lengthOfAcrossClues.append([clueNumber, wordLength])
+    
+    def webScrap(self):
+        print("TODO")
+        textFromWeb = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+        self.acrossClueDomains = {"1":textFromWeb,"5":textFromWeb ,"6":textFromWeb ,"7":textFromWeb ,"8":textFromWeb}
+        self.downClueDomains = {"1":textFromWeb,"2":textFromWeb ,"3":textFromWeb ,"4":textFromWeb ,"6":textFromWeb}
+
+    def filterDomains(self):
+        #downClues
+        newDomain = []
+        for clue in self.lengthOfDownClues:
+            for word in self.downClueDomains[clue[0]].split():
+                if len(word) == clue[1]:
+                    newDomain.append(word)
+            self.downClueDomains[clue[0]] = newDomain
+            newDomain = []
+
+         #acrossClues
+        newDomain = []
+        for clue in self.lengthOfAcrossClues:
+            for word in self.acrossClueDomains[clue[0]].split():
+                if len(word) == clue[1]:
+                    newDomain.append(word)
+            self.acrossClueDomains[clue[0]] = newDomain
+            newDomain = []
 
 
-            
-
-
-
-
-
-textFromWeb = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
 
 
 
