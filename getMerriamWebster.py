210d991a3567824f504274b975c614df61398af7
Ata Atasoy <h.ataatasoy@gmail.com>
2020-12-14 01:07:02 +0300

    Implement clue tokenizing and modify search

--- getMerriamWebster.py
+++ getMerriamWebster.py
@@ -1,26 +1,29 @@
+import nltk 
 import requests
 from bs4 import BeautifulSoup
 
 URL = "https://www.merriam-webster.com/dictionary/"
 
 def get_merriam_webster(query):
-
-    request_url = URL + query
-    page = requests.get(request_url)
-    source = page.text
-    soup = BeautifulSoup(source, 'html.parser')
-    for span in soup.find_all("span", class_="ex-sent first-child t no-aq sents"):
-        span.decompose()
-
-    results = soup.find_all("span", class_="dtText")
-
+    # Tokenize the search query (clue)
+    words = nltk.word_tokenize(query)
     string_results = list()
-    for result in results:
-        result = result.text
-        result = result[1:]
-        result = result.replace("\n", "")
-        result = result.replace("\t", "")
-        result = result.strip()
-        string_results.append(result)
+
+    for word in words:
+        request_url = URL + word
+        page = requests.get(request_url)
+        source = page.text
+        soup = BeautifulSoup(source, 'html.parser')
+        for span in soup.find_all("span", class_="ex-sent first-child t no-aq sents"):
+            span.decompose()
+    
+        results = soup.find_all("span", class_="dtText")
+
+        for result in results:
+            result = result.text
+            result = result[1:]
+            result = result.replace("\n", "")
+            result = result.replace("\t", "")
+            string_results.append(' '.join(result.split()))
     
     return " ".join(string_results)
