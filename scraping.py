4ef58d8d9ff214227584ce5492ea763c7cfe7608
Mali <altunsoymali@gmail.com>
2020-12-14 09:13:45 +0300

    the format of scraping class is modified

--- scraping.py
+++ scraping.py
@@ -1,9 +1,6 @@
-import requests
-from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import wikipedia
-import time
+from getMerriamWebster import get_merriam_webster
+from searchWikipedia import searchWikipediaApi
+from synonyms import searchSynonyms
 
 class Scraping:
     def __init__(self, clues, answers, gridIndex):
@@ -39,56 +36,13 @@ class Scraping:
         return "toSearch"
 
     def getWiki(self, toSearch):
-        search_results = wikipedia.search(toSearch, results=2)
-        text = ""
-        for result in search_results:
-            text += wikipedia.page(result + ".").content
-        return text
+        return searchWikipediaApi(toSearch)
 
     def getMerriam(self,toSearch):
-        request_url = "https://www.merriam-webster.com/dictionary/" + toSearch
-        page = requests.get(request_url)
-        source = page.text
-        soup = BeautifulSoup(source, 'html.parser')
-        for span in soup.find_all("span", class_="ex-sent first-child t no-aq sents"):
-            span.decompose()
-
-        results = soup.find_all("span", class_="dtText")
-
-        string_results = list()
-        for result in results:
-            result = result.text
-            result = result[1:]
-            result = result.replace("\n", "")
-            result = result.replace("\t", "")
-            result = result.strip()
-            string_results.append(result)
-        
-        return " ".join(string_results)
+        return get_merriam_webster(toSearch)
 
     def getSynonyms(self, toSearch):
-        words = set()
-        for word in toSearch.split():
-            r = requests.get("https://www.merriam-webster.com/thesaurus/" + word)
-            soup = BeautifulSoup(r.content, 'html.parser')
-
-            for i in soup.select('.thes-list.syn-list a'):
-                words.add(i.string)
-
-            for i in soup.select('.thes-list.rel-list a'):
-                words.add(i.string)
-                
-            for i in soup.select('.thes-list.sim-list a'):
-                words.add(i.string)
-            
-            r = requests.get(
-                'https://api.datamuse.com/words?rel_syn={}'
-                .format(word))
-            results = r.json()
-            for result in results:
-                words.add(result['word'])
-
-        return ' '.join(words)
+        return searchSynonyms(toSearch)
 
     def cheat(self):
         for across in self.clues["across"]:
