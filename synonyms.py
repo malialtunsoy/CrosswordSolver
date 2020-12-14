b224c16274ad62bc1c12320e1c316ca88062bd8f
busrabgz <busrabgz@gmail.com>
2020-12-14 14:29:57 +0300

    Add thesaurus.com to search synonyms

--- synonyms.py
+++ synonyms.py
@@ -1,12 +1,47 @@
 import requests
 from time import sleep
 from bs4 import BeautifulSoup
+import string
 
 def searchSynonyms(clue_words):
     words = set()
     for word in clue_words:
-        r = requests.get("https://www.merriam-webster.com/thesaurus/" + word)
-        soup = BeautifulSoup(r.content, 'html.parser')
+        if(contains_multiple_words(word)):
+            # Thesaurus.com
+            r_th = requests.get("https://www.thesaurus.com/browse/" + word.split()[0] + "%20" + word.split()[1])
+
+            # Merriam webster
+            r_mw = requests.get("https://www.merriam-webster.com/thesaurus/" + word.split()[0] + "%20" + word.split()[1])
+
+            # Datamuse
+            r_dm = requests.get('https://api.datamuse.com/words?rel_syn={}_{}'.format(word.split()[0], word.split()[1]))
+
+        else:
+            # Thesaurus.com
+            r_th = requests.get("https://www.thesaurus.com/browse/" + word)
+
+            # Merriam webster
+            r_mw = requests.get("https://www.merriam-webster.com/thesaurus/" + word)
+
+            # Datamuse
+            r_dm = requests.get('https://api.datamuse.com/words?rel_syn={}'.format(word))
+
+       
+        soup = BeautifulSoup(r_th.text, 'html.parser')    
+        texts = soup.findAll(text=True)
+        punc = False
+        for w in texts:
+            if ("WORD GAMES" in w) or ("Crossword" in w):
+                continue
+            for c in w:
+                if c in string.punctuation:
+                    punc = True
+                    break
+            if punc == False:
+                words.add(w)
+            punc = False
+            
+        soup = BeautifulSoup(r_mw.content, 'html.parser')
 
         for i in soup.select('.thes-list.syn-list a'):
             words.add(i.string)
@@ -15,15 +50,16 @@ def searchSynonyms(clue_words):
             words.add(i.string)
             
         for i in soup.select('.thes-list.sim-list a'):
-            words.add(i.string)
+            words.add(i.string)        
         
-        r = requests.get(
-            'https://api.datamuse.com/words?rel_syn={}'
-            .format(word))
-        results = r.json()
+        results = r_dm.json()
         for result in results:
             words.add(result['word'])
 
     return ' '.join(words)
+
+def contains_multiple_words(s):
+  return len(s.split()) > 1
+
     
-#print(searchSynonyms(["historical", "artifact"]))
+print(searchSynonyms(["historical", "artifact"]))
