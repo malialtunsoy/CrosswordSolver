5ee5719e6096d66c16600e5a46639eaff27c2920
Ata <h.ataatasoy@gmail.com>
2020-12-22 16:46:03 +0300

    fix imports

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from requests.api import delete
 from scraping import Scraping
 from crosswordSolver import CrosswordSolver
 from Connector import Connector
-from NewSolver import NewSolver
+from newSolver import NewSolver
 from PyQt5 import QtCore, QtGui, QtWidgets, Qt
 from crossword_gui import Ui_MainWindow
 import json
@@ -37,7 +37,7 @@ class LUMOSCrosswordSolver:
                 self.clues, self.cellAnswerArray, self.cellNumberArray)
             webScrapper.setDomains()
             print("===================\nSOLVING THE PUZZLE\n===================")
-            puzzleSolver = newSolver(self.cellBlockArray, self.cellNumberArray,
+            puzzleSolver = NewSolver(self.cellBlockArray, self.cellNumberArray,
                                      self.cluesDown, self.cluesAcross, webScrapper.domains)
         else:
 
@@ -58,7 +58,7 @@ class LUMOSCrosswordSolver:
             webScrapper = Scraping(self.clues, self.cellAnswerArray, self.cellNumberArray)
             webScrapper.setDomains()
             print("===================\nSOLVING THE PUZZLE\n===================")
-            puzzleSolver = newSolver(self.cellBlockArray, self.cellNumberArray,self.cluesDown, self.cluesAcross, webScrapper.domains)
+            puzzleSolver = NewSolver(self.cellBlockArray, self.cellNumberArray,self.cluesDown, self.cluesAcross, webScrapper.domains)
 
         # SAVE
         """
